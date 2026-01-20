a2d4057d7e6526ddae0079ae56df4b9e178b4ac8
Raj Mane <rajmane5884@gmail.com>
2026-01-20 10:10:19 +0530

    init

---
+++ tm-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-white tracking-tight">Hi there!</h1>
+    </div>
+  );
+}
